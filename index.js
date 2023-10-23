24ea602eb26d0f97b837159af6075fb0c9f05ea8
RomelSikdar <romels500@gmail.com.com>
2023-10-23 20:46:59 +0530

    miscellaneous updates

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const mount = require("koa-mount");
 const serve = require("koa-static");
 
 module.exports = (server) => {
-  server.use(mount("/admin", serve(__dirname + "/build")));
+  server.use(mount("/admin", serve(__dirname + "/dist")));
 };
