af1f17f8df8521bc4c961f54d812909277064f90
RomelSikdar <romels500@gmail.com.com>
2023-10-23 21:19:07 +0530

    Router added

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/admin",
+  server: {
+    port: 3000,
+    proxy: {
+      "/editor/flow": {
+        target: "http://localhost:3001/",
+        changeOrigin: true,
+      },
+      "/editor/schema": {
+        target: "http://localhost:3002/",
+        changeOrigin: true,
+      },
+    },
+  },
 });
