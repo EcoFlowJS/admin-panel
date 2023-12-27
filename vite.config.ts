96e30f29516df943638a4217fe155eb72d6d0ace
RomelSikdar <romels500@gmail.com.com>
2023-12-27 21:05:37 +0530

    Landing page package added and vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
-import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ babel:{plugins:[jotaiDebugLabel,jotaiReactRefresh]}})],
+  plugins: [
+    react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
+  ],
   base: "/admin",
   server: {
-    open: true,
-    port: 3000,
+    port: 3001,
     proxy: {
+      "/auth": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
       "/editor/flow": {
-        target: "http://localhost:3001/",
+        target: "http://localhost:3002/",
         changeOrigin: true,
       },
       "/editor/schema": {
-        target: "http://localhost:3002/",
+        target: "http://localhost:3003/",
         changeOrigin: true,
       },
     },
   },
 });
-
