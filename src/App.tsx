86940f8f038202e75b213f4950c72adcb5cf9c7c
RomelSikdar <romels500@gmail.com.com>
2024-01-03 08:37:55 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { atom, getDefaultStore } from "jotai";
 import router from "./lib/router/BrowserRouter";
-
-const defaultStore = getDefaultStore();
-console.log(defaultStore.get(atom("abcef")));
 
 export default router;
--- src/pages/setup/setup.page.tsx
+++ src/pages/setup/setup.page.tsx
@@ -1,7 +1,5 @@
 import BaseSetup from "../../components/setup/base/base.component";
 
 export default function SetupPage() {
-  return (
-    <BaseSetup />
-  )
+  return <BaseSetup />;
 }
